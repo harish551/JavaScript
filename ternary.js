a3be75e5852680ec1bb102cdea400e6971d9dafd
Harish <harishmarri551@gmail.com>
2018-12-22 15:36:11 +0530

    intial commit

---
+++ ternary.js
@@ -0,0 +1,4 @@
+let num = 4; 
+(num%2==0 )? console.log("Even Number"): console.log("Odd number"); //Even Number 
+let age = 17;
+(age>=18)?console.log("You have right to vote"):console.log("you can't vote!!"); //you can't vote
