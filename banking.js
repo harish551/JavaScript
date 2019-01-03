ee85b0e865112720594ed8936e6817e7d54ffc1d
Harish <harishmarri551@gmail.com>
2019-01-03 14:17:30 +0530

    intial commit

--- banking.js
+++ banking.js
@@ -1,28 +1,24 @@
-let bank = (function(){
+let bank = (function () {
+  return {
+    balance: 0,
+    showBalance: function () {
+      console.log('Balance: ' + this.balance)
+    },
+    deposit: function (money) {
+      this.balance += money
+      console.log(money + ' deposited')
+      console.log('Available balace: ' + this.balance)
+    },
+    withdraw: function (money) {
+      this.balance -= money
+      console.log(money + ' withdrawed')
+      console.log('Available balance: ' + this.balance)
+    }
+  }
+})()
 
-	return {
-		balance : 0,
-		showBalance : function(){
-			console.log("Balance: "+ this.balance);
-		},
-		deposit: function(money){
-			this.balance+=money;
-			console.log(money + " deposited");
-			console.log("Available balace: "+ this.balance);
-	},
-		withdraw : function(money){
-			this.balance -= money;
-			console.log(money + " withdrawed");
-			console.log("Available balance: " + this.balance);
-	}
-};
+bank.showBalance()
+bank.deposit(10000)
+bank.withdraw(100)
 
-})();
-
-bank.showBalance();
-bank.deposit(10000);
-bank.withdraw(100);
-
-bank.showBalance();
-
-
+bank.showBalance()
